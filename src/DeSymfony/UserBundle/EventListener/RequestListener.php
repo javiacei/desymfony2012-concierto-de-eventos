dcf7f094eefc54435bc1abbdbab25ccd0b4c5539
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2012-06-01 21:18:46 +0200

    #5 - Inyectado el servicio logger en el Listener

--- src/DeSymfony/UserBundle/EventListener/RequestListener.php
+++ src/DeSymfony/UserBundle/EventListener/RequestListener.php
@@ -3,10 +3,19 @@
 namespace DeSymfony\UserBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Log\LoggerInterface;
 
 class RequestListener
 {
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onKernelRequest(GetResponseEvent $event)
     {
+        $this->logger->info("Evento notificado **kernel.request** por el bundle DeSymfonyUserBundle");
     }
 }
