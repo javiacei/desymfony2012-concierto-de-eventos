94dd90275febafa49feca21f26ac4b9aeb32f63c
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2012-06-02 14:12:32 +0200

    El gestor de usuarios lanza el evento pre_user_save

--- src/DeSymfony/UserBundle/Event/GetUserEvent.php
+++ src/DeSymfony/UserBundle/Event/GetUserEvent.php
@@ -5,7 +5,7 @@ namespace DeSymfony\UserBundle\Event;
 use Symfony\Component\EventDispatcher\Event;
 use DeSymfony\UserBundle\Entity\User;
 
-class GetUserEvent
+class GetUserEvent extends Event
 {
     protected $user;
 
--- src/DeSymfony/UserBundle/Model/UserManager.php
+++ src/DeSymfony/UserBundle/Model/UserManager.php
@@ -3,6 +3,7 @@
 namespace DeSymfony\UserBundle\Model;
 
 use DeSymfony\UserBundle\Entity\User;
+use DeSymfony\UserBundle\Event\GetUserEvent;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -26,7 +27,8 @@ class UserManager
         $user = new User();
         $user->setEmail($email);
 
-
+        $event = new GetUserEvent($user);
+        $this->dispatcher->dispatch('desymfony.pre_user_save', $event);
 
         $this->em->persist($user);
         $this->em->flush();
