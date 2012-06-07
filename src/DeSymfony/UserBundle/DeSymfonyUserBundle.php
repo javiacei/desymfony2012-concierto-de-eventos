d6d6884e0be674834bcc2541d5a512b9a6305e16
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2012-06-07 18:56:58 +0200

    #0 - Punto de arranque

--- src/DeSymfony/UserBundle/DeSymfonyUserBundle.php
+++ src/DeSymfony/UserBundle/DeSymfonyUserBundle.php
@@ -1,10 +1,10 @@
 <?php
 
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
 namespace DeSymfony\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use DeSymfony\UserBundle\DependencyInjection\Compiler\RegisterUserListenersPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class DeSymfonyUserBundle extends Bundle
 {
